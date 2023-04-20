f7df83993e6d5f5788b0f9ff30678a3d49b02b18
liyanxun <liyanxun1128@gmail.com>
2023-04-19 19:02:05 -0700

    hw3

--- homework3/buildkite/ui/build_ui.sh
+++ homework3/buildkite/ui/build_ui.sh
@@ -9,10 +9,10 @@ $DOCKER_PASSWORD=$DOCKER_PASSWORD
 docker build -f ./homework2/docker/ui.Dockerfile -t ui:latest ./homework1/ui
 
 # tag image
-docker tag ui:latest $DOCKER_REPO:latest
+docker tag ui:latest $DOCKER_USERNAME/$DOCKER_REPO:latest
 
 # docker login
 docker login --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD
 
 # publish image
-docker push $DOCKER_REPO:latest
+docker push $DOCKER_USERNAME/$DOCKER_REPO:latest
